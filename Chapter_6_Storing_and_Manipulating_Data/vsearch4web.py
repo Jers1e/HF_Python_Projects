1c42b1d95240546d08e01d402d77501f0c5d3e33
Jers1e <128065477+Jers1e@users.noreply.github.com>
2023-03-30 16:14:50 +0000

    Fixed HTML formatting not working

--- Chapter_6_Storing_and_Manipulating_Data/vsearch4web.py
+++ Chapter_6_Storing_and_Manipulating_Data/vsearch4web.py
@@ -4,12 +4,14 @@ import dunder
 
 app = Flask(__name__)
 
+
 def log_request(req: 'flask_request', res: str) -> None:
     with open('vsearch.log', 'a') as log:
         print(req, res, file=log)
 
+
 @app.route('/search4', methods=['POST'])
-def do_search() -> str:
+def do_search() -> 'html':
     phrase = request.form['phrase']
     letters = request.form['letters']
     title = 'Here are your results: '
@@ -17,16 +19,19 @@ def do_search() -> str:
     log_request(request, results)
     return render_template('results.html', the_title=title, the_phrase=phrase, the_letters=letters, the_results=results)
 
+
 @app.route('/')
 @app.route('/entry')
 def entry_page() -> 'html':
     return render_template('entry.html', the_title='Welcome to search4letters on the web!')
 
+
 @app.route('/viewlog')
-def view_the_log() -> str:
+def view_the_log() -> 'html':
     with open('vsearch.log') as log:
         contents = log.read()
     return contents
+
 
 if __name__ == '__main__':
     app.run(debug=True)
